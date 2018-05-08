99d2d54eb738557647af19b78a660b083cdea2fc
Vierageorge <joviera@gmail.com>
2018-05-08 05:02:22 -0500

    Added a DOC to a Class

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Classes
+In this files I'll just be messing around with OOP on Python. Comments on the code.
--- classHelloWorld.py
+++ classHelloWorld.py
@@ -1,5 +1,7 @@
 class Car:
-    def __init__(self, wheels):
-        self.__wheels = wheels
+    '''A simple Car class.'''
+    __wheels=2
+    #def __init__(self, wheels):
+    #    self.__wheels = wheels
     def getWheels(self):
         return self.__wheels
